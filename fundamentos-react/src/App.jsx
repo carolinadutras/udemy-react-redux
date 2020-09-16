2dccd83d2a50a26355c296e82d02f582f7afc91b
carolinadutras <carolina.dutras@gmail.com>
2020-09-16 17:01:53 -0300

    map in child components

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,12 +5,22 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragment";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
+import Familia from './components/basicos/Familia';
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 
 export default function App(props) {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
+      <Card titulo=" #4 - Componentes com filhos" color= '#00BBFF'> 
+      <Familia sobrenome= 'Dutra' >
+         <FamiliaMembro nome='Angela' 
+      />
+      <FamiliaMembro nome='Carol' />
+      <FamiliaMembro nome= 'Duda' />
+      </Familia>
+        </Card>
 
         <Card titulo=" #4 - Desafio Aleatório" color= '#FF5733'> 
           <Aleatorio min={10} max={60} />
--- fundamentos-react/src/components/basicos/Familia.jsx
+++ fundamentos-react/src/components/basicos/Familia.jsx
@@ -1,1 +1,22 @@
-import React from 'react';
+import React ,{cloneElement}from "react";
+import FamiliaMembro from "./FamiliaMembro";
+export default (props) => {
+    console.log(typeof props.children.map)
+  return (
+    <div>
+        
+        {props.children.map((child, i)=> {
+            return cloneElement(child, {...props, key: i })
+        })}
+
+
+        {/* {cloneElement(props.children, props)} */}
+        {/* {Array(10).fill(7)} */}
+        {/* {30*10} */}
+      {/* <FamiliaMembro nome='Angela' sobrenome=
+      {props.sobrenome}/>
+      <FamiliaMembro nome='Carol' {...props}/>
+      <FamiliaMembro nome= 'Duda' sobrenome='Chiosi'/> */}
+    </div>
+  );
+};
