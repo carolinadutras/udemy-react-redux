086ba490249df5fec81f0310454b2d411fb696e2
carolinadutras <carolina.dutras@gmail.com>
2020-09-17 09:05:22 -0300

    exercicio repetiçao

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,44 +1,47 @@
 import React from "react";
 import "./App.css";
-import ListaAlunos from './components/repeticao/ListaAlunos';
+import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragment";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
-import Familia from './components/basicos/Familia';
-import FamiliaMembro from './components/basicos/FamiliaMembro'
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 export default function App(props) {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <div className="Cards">
-      <Card titulo=" #6 - Repetição " color= '#FFC300'> 
-<ListaAlunos/>
+      <Card titulo=" #7 - Exercicio Repetição  " color="#FFC300">
+          <TabelaProdutos />
+        </Card>
+        <Card titulo=" #6 - Repetição " color="#FFC300">
+          <ListaAlunos />
         </Card>
 
-      <Card titulo=" #5 - Componentes com filhos" color= '#00BBFF'> 
-      <Familia sobrenome= 'Dutra' >
-         <FamiliaMembro nome='Angela' 
-      />
-      <FamiliaMembro nome='Carol' />
-      <FamiliaMembro nome= 'Duda' />
-      </Familia>
+        <Card titulo=" #5 - Componentes com filhos" color="#00BBFF">
+          <Familia sobrenome="Dutra">
+            <FamiliaMembro nome="Angela" />
+            <FamiliaMembro nome="Carol" />
+            <FamiliaMembro nome="Duda" />
+          </Familia>
         </Card>
 
-        <Card titulo=" #4 - Desafio Aleatório" color= '#FF5733'> 
+        <Card titulo=" #4 - Desafio Aleatório" color="#FF5733">
           <Aleatorio min={10} max={60} />
         </Card>
 
-        <Card titulo="#3 - Fragmentos" color= '#581845'>
+        <Card titulo="#3 - Fragmentos" color="#581845">
           <Fragmento />
         </Card>
 
-        <Card titulo="#2 -  Com Parametro" color ='#FFC300'>
+        <Card titulo="#2 -  Com Parametro" color="#FFC300">
           <ComParametro titulo="Média do aluno" nota={9.1} aluno="Pedro" />{" "}
         </Card>
-        <Card titulo=" #1 - Primeiro Componente" color = '#33FFBD'>
+        <Card titulo=" #1 - Primeiro Componente" color="#33FFBD">
           <Primeiro />
         </Card>
       </div>
--- fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
+++ fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
@@ -1,2 +1,29 @@
 import React from 'react';
 import produtos from '../../data/produto';
+
+export default (props)=>{
+
+const renderLista = produtos.map((produto)=>{
+return(<div>
+
+<tr key={produto.id} style={{border:'solid 1px '}}> 
+<td >
+{produto.id} -
+</td>
+<td>
+{produto.nome} - 
+</td>
+<td>
+{produto.preco}
+</td>
+</tr>
+
+</div>)
+})
+return(
+    <div>
+        {renderLista}
+    </div>
+
+)
+}
